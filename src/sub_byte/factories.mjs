bbd38af3f3a9754486d989fa29a355ec9c19e193
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-10-20 22:16:52 +0100

    Partial revamp

--- src/sub_byte/factories.mjs
+++ src/sub_byte/factories.mjs
@@ -5,10 +5,21 @@ const GetBits = function(x) {
     return x.toString(2)
 } 
 
+export class ValueSet extends Set {
+    Natural
+};
 
-export const MakeSubByteEncoderAndDecoder = function(symbols) {
+
+const MakeSubByteEncoderAndDecoder = function(symbols) {
 
     const unique_symbols = new Set(symbols);
+    if (unique_symbols.size <= 1) {
+        throw new Error('All symbols are the same, or no symbols have been given, so there '
+                       +'is little point in using this library - just count them and '
+                       +'transmit the total number of symbols.'
+                       +`Set(symbols): ${unique_symbols}` 
+                       );
+    }
     const decodings = Array.from(unique_symbols.values());
 
     const encodings = Object.fromEntries(decodings.entries().map(([i,s]) => [s,i]));
@@ -18,42 +29,84 @@ export const MakeSubByteEncoderAndDecoder = function(symbols) {
     }
 
     const bits_per_symbol = GetBits(unique_symbols.size - 1).length;
-    const num_symbols_per_byte = Math.floor(8 / bits_per_symbol);
 
-    if (num_symbols_per_byte <= 0) {
-        throw new Error('More than one byte needed to uniquely encode each symbol. '
-                       +' Use a byte stream encoding library instead, e.g. protobuf.dev'
+    
+    if (bits_per_symbol <= 0) {
+        throw new Error(`Internal error: bits per symbol: ${bits_per_symbol}. `
+                       +`Unique symbols: ${unique_symbols}` 
+                       +'The symbols given each require zero bits to encode.'
                        );
     }
 
-    const encoder = function* (symbols_iterator) {
-        // let num = 0;
-        // let bits_used = 0
-        for (let result = symbols_iterator.next(); 
-             result.done===false;
-             result = symbols_iterator.next()) {
+    const num_symbols_per_byte = Math.floor(8 / bits_per_symbol);
+
+    // if (num_symbols_per_byte <= 0) {
+    //     throw new Error('More than one byte needed to uniquely encode each symbol. '
+    //                    +' Use a byte stream encoding library instead, e.g. protobuf.dev'
+    //                    );
+    // }
+
+    const encoder = function* (symbols_data) {
+
+        // If symbols doesn't have Symbol.iterator, just try to
+        // use symbols.
+        const iterator = symbols_data?.[Symbol.iterator]() || symbols_data;
+
+        // Initialise a binary buffer (num, an ordinary Number) 
+        // and a bit counter.
+        let num = 0;
+        let bits_used = 0;
+
+        // This could be rewritten as a for...of loop over iterator, 
+        // as we have already reference to it.  But multiple explicit 
+        // calls to .next are easier to debug, (also called in 
+        // the inner while loop) and emphasises the intent, without
+        // requiring knowledge of JS deep magic.
+        for (let result = iterator.next(); 
+             !result.done === false;
+             result = iterator.next()) {
             
-            let num = encodings[result.value];
-            // num <<= bits_per_symbol;
-            // num += encodings[result.value];
-            // bits_used += bits_per_symbol;
+            // let num = encodings[result.value];
 
-            const rest_of_symbols_in_this_byte = symbols_iterator.take(num_symbols_per_byte-1);
+            // const rest_of_symbols_in_this_byte = iterator.take(num_symbols_per_byte-1);
 
 
-            rest_of_symbols_in_this_byte.forEach((symbol) => {
+            // rest_of_symbols_in_this_byte.forEach((symbol) => {
+            //     num <<= bits_per_symbol;
+            //     num += encodings[symbol];
+            //     // bits_used += bits_per_symbol;
+            // });
+
+
+            while (bits_used < 8 && result.done === false) {
+                // Left bitshift to make room for next symbol's encoding.
                 num <<= bits_per_symbol;
-                num += encodings[symbol];
-                // bits_used += bits_per_symbol;
-            });
+                // Add in the next symbol's encoding (overwriting  
+                // the zero bits just created).
+                num += encodings[result.value];
+                bits_used += bits_per_symbol;
 
-            // bits_used -= 8;
-            // yield num >> bits_used
-            // num = (num << (8 - bits_used)) & 255 
+                // Get next symbol
+                result = iterator.next()
+            }
+
+            // yield num;
+
+            // Yield encoded bytes from the buffer
+            while (bits_used >= 8) {}
+                // subtract bits to be yielded from counter, and yield them
+                bits_used -= 8;
+                yield (num >> bits_used) & 255;
+            // Bit shift any remaining unyielded bits that don't fill 
+            // an entire byte to the highest order bits (128, 64, 32 etc.)
+            num = (num << (8 - bits_used)) & 255 
+
+        }
+
+        // Clear the buffer of any encoded symbols, that were too few 
+        // to completely fill the buffer.
+        if (bits_used >= 1) {
             yield num;
-
-
-            // num &
         }
 
     };
@@ -104,3 +157,4 @@ export const MakeSubByteEncoderAndDecoder = function(symbols) {
     return [encoder, decoder, bits_per_symbol, num_symbols_per_byte];
 }
 
+export default MakeSubByteEncoderAndDecoder;
