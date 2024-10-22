6a1e9e6890ca41e535d4014b4842a10008c7149c
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-10-22 18:20:51 +0100

    Homebrew fuzzer.  Cant run on more than 19 weirdly (hardware constraint?).  Tests pass

--- tests/tests.mjs
+++ tests/tests.mjs
@@ -9,7 +9,9 @@ import {intEncoder, intDecoder} from '../src/sub_byte/factories.mjs';
 
 
 
-
+function randInt(x) {
+    return Math.floor(Math.random() * x);
+  }
 
 describe('round_trip', function () {
     const tests = [
@@ -18,6 +20,20 @@ describe('round_trip', function () {
         {integers: [0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1,0,0,0,0,0,1,1,0,1], bit_widths: [1]},
         {integers: [1000, 2,1234], bit_widths: [10,2,11]},
     ];
+
+    for (let i = 0; i < 16; i++) {
+        const n = randInt(50);
+        const integers = [];
+        const bit_widths = [];
+        for (let j=0;  j < n; j++) {
+            const integer = randInt(1000000);
+            const bit_width = integer.toString(2).length + randInt(4);
+            
+            integers.push(integer)
+            bit_widths.push(bit_width);
+        }
+        tests.push({integers: integers, bit_widths: bit_widths})
+    }
   
     tests.forEach(({integers, bit_widths}) => {
       it(`correctly roundtrips ${integers} using widths: ${bit_widths}`, function () {
