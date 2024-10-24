aaa2b74778f90b3479e933048dd7d6244b5975b5
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-10-24 10:15:10 +0100

    Update ops_and_seeds_codecs.mjs

--- tests/ops_and_seeds_codecs.mjs
+++ tests/ops_and_seeds_codecs.mjs
@@ -1,4 +1,5 @@
 import SYMBOLS from "./symbols.json" with { type: "json" };
+// const SYMBOLS = await import('./symbols.json', { with: { type: "json" } });
 
 import { MakeSubByteEncoderAndDecoder } from "../src/sub_byte/factories.mjs";
 
@@ -7,7 +8,7 @@ const ALL_SEEDS = SYMBOLS.SEEDS;
 // Sets are not necessary for MakeSubByteEncoderAndDecoder (it will
 // create a set itself).  They're just to make writing the
 // tests a little easier.
-export const UNIQUE_SEEDS = new Set(SYMBOLS.SEEDS.map((x) => x.toString()));
+export const UNIQUE_SEEDS = new Set(ALL_SEEDS.map((x) => x.toString()));
 export const OPS = new Set(SYMBOLS.OPS);
 
 export const [encodeOps, decodeOps, opsBitWidths, opsEncodings, opsDecodings] =
