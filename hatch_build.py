a67a2ce8926414af8480396377124cf333b16a82
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-02-18 10:23:22 +0000

    Dynamically generate project scripts from __main__.py files in codecs

--- hatch_build.py
+++ hatch_build.py
@@ -6,7 +6,8 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class CustomHook(BuildHookInterface):
     def initialize(self, version, build_data):
-        build_data['scripts'] = scripts = []
+        
+        script = build_data.setdefault('scripts', {})
 
         src = pathlib.Path(__file__).parent / 'src'
         codecs  =  src / 'sub_byte' / 'codecs'
@@ -14,4 +15,6 @@ class CustomHook(BuildHookInterface):
         for codec in codecs.iterdir():
             for dir_ in ['encode', 'decode']:
                 if (codec / dir_ / '__main__.py').is_file():
-                    scripts.append(f'sub_byte.codecs.{dir_}.__main__:main')
+                    script = f'sub_byte.codecs.{dir_}.__main__:main'
+                    for c in ['_','-']:
+                        scripts[f'{dir_}{c}{codec.name}'] = script
