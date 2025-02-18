122f8945f7c83a7d9f44feaa5471cecbcded1bce
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-02-18 10:16:27 +0000

    Dynamically generate scripts

---
+++ hatch_build.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class CustomHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        build_data['scripts'] = scripts = []
+
+        src = pathlib.Path(__file__).parent / 'src'
+        codecs  =  src / 'sub_byte' / 'codecs'
+
+        for codec in codecs.iterdir():
+            for dir_ in ['encode', 'decode']:
+                if (codec / dir_ / '__main__.py').is_file():
+                    scripts.append(f'sub_byte.codecs.{dir_}.__main__:main')
